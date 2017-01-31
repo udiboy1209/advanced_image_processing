b283f745a38f871ccccd7b177b61bd9bb7f8538f
Meet Udeshi <mudeshi1209@gmail.com>
2017-02-01 03:30:51 +0530

    Finish q2 gaussian fitting

--- hw1/q2/integration.m
+++ hw1/q2/integration.m
@@ -1,19 +1,34 @@
-dt = 10^-3;
-b = 1;
-t = dt:dt:b; % 0 -> sqrt(b)
+dt = 10^-1;
+b = 10;
+t = dt:dt:sqrt(b); % 0 -> sqrt(b)
 
-dI = 10^-3;
+dI = 10^-2;
 
-Ivals = 0:dI:1;
+Ivals = 0:dI:10;
 PI = zeros(size(Ivals));
 
 for i = 1:size(Ivals,2)
     I = Ivals(i);
     val = sum(exp(bsxfun(@rdivide,-I^2,(2*t)))./sqrt(t) * dt);
-    PI(i) = val/sqrt(2*pi)/(b);
+    PI(i) = val/sqrt(2*pi)/sqrt(b);
 end
 
+intg = arrayfun(@(I) dct_distribution_uniform_var(0,sqrt(1000),10000,I), Ivals);
+
+%figure;
+%plot(Ivals,[intg]);
+
 PI = [fliplr(PI) PI(2:end)];
+% PI = PI/sum(PI);
 Ivals = [-fliplr(Ivals) Ivals(2:end)];
-plot(Ivals,PI)
+
+%lnpi = log(PI);
+%coeffs = polyfit(Ivals, lnpi, 2)
+
+g = fit(Ivals.', PI.','gauss1');
+% Gvals = exp(coeffs(1)*Ivals.^2 + coeffs(3));
+% Gvals = 1/(sqrt(2*pi)*1.05)*exp(-Ivals.^2/(2*(1.05)^2));
+Gvals = g(Ivals);
+figure;
+plot(Ivals,[PI;Gvals']);
     
