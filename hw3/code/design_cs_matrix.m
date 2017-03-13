b2710d2206155a7a03539933169b1a64088d17f2
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-03-14 02:24:15 +0530

    Made minimal changes to the code

--- hw3/code/design_cs_matrix.m
+++ hw3/code/design_cs_matrix.m
@@ -11,7 +11,8 @@ function [psi,phi_o, phi_d, normE] = design_cs_matrix()
     r = sum(lambda_vec ~= 0);
     
     tau_cap = phi_cap * V;
-    normE = zeros(m,1);
+%     normE = zeros(m,1);
+%     norm_initial = zeros(m,1);
     %tau_res = zeros(size(tau_cap));
     
     for j = 1:m
@@ -19,11 +20,15 @@ function [psi,phi_o, phi_d, normE] = design_cs_matrix()
         tau_j = tau_cap(j,:);
         vj = (tau_j').*lambda_vec;
         Ej = E + vj * vj';
-        [u1,e1] = eigs(Ej,1);
+        [u,s,v] = svd(Ej);
+%         [u1,e1] = eigs(Ej,1);
         %No idea why e1 is becoming negative???
-        vj_new = sign(e1)*sqrt(abs(e1))*u1;
-        
-        normE(j) = norm(Ej - vj_new'*vj_new);
+%         vj_new = sign(e1)*sqrt(abs(e1))*u1;
+        vj_new = sqrt(s(1,1))*u(:,1);
+        norm_initial = norm(E(:));
+%         E_new = Ej - vj_new*vj_new';
+        E_new = Ej - s(1,1)*u(:,1)*v(:,1)';
+        normE = norm(E_new(:));
         tau_cap(j,ind) = vj_new(ind)./lambda_vec(ind);
     end
     phi_d = tau_cap * V';   
