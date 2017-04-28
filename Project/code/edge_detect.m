4c608ef9e318c336c09d6d79815223842e8d2658
Meet Udeshi <mudeshi1209@gmail.com>
2017-04-28 07:19:36 +0530

    I_A calculation and usage done

--- Project/code/edge_detect.m
+++ Project/code/edge_detect.m
@@ -1,6 +1,7 @@
 close all;
 % dataset_format = '../data/junction_dataset/image_00/data/%.10d.png';
 dataset_format = 'datasets/dataset_cannon/synth_bg_sub/img%d.png';
+% dataset_format = 'datasets/own_dataset/canon_images_stable/%.3d.png';
 
 si = 1000;
 ei = 2000;
@@ -12,9 +13,22 @@ color = 1:3;
 [u1_vals,u2_vals,I_vals] = get_skp_from_img(double(imread(sprintf(dataset_format,1))));
 [m1,c1,m2,c2] = linefit(u1_vals,u2_vals,I_vals);
 
-img1 = double(imread(sprintf(dataset_format,randi([450,550]))));
+I_all = repmat(0:255,[3,1])';
+u1_all = repmat(m1,[256,1]).*I_all + repmat(c1,[256,1]);
+u2_all = repmat(m2,[256,1]).*I_all + repmat(c2,[256,1]);
+
+I_A = get_Ia(u1_all,u2_all,1e-5);
+
+img1 = imread(sprintf(dataset_format,randi([450,650])));
+img1_Ia = zeros(size(img1));
 
 [m,n,c] = size(img1);
+
+for i = 1:c
+    img1_Ia(:,:,i) = arrayfun(@(v) I_A(v,i), img1(:,:,i));
+end
+img1 = double(img1);
+
 I_x = zeros(m,n);
 I_y = zeros(m,n);
 
@@ -27,28 +41,30 @@ edge_img = logical(edge_img ~= 0);
 d_x = img1_padded_x(1:m,1:n,:) - img1;
 d_y = img1_padded_y(1:m,1:n,:) - img1;
 
+edge_mask_x = abs(d_x)>img1_Ia;
+edge_mask_y = abs(d_y)>img1_Ia;
+    
+
 for cidx = 1:numel(color)
     c = color(cidx);
     
-    u1 = m1(c)*img1(:,:,c) + c1(c);
-    u2 = m2(c)*img1(:,:,c) + c2(c);
-    bessel = bsxfun(@besseli,abs(d_x(:,:,c)),2*sqrt(u1.*u2));
-
-    p = exp(-u1-u2).*(u1./u2).^(d_x(:,:,c)/2).*bessel;
-    edge_mask = p < 5e-10;
+%     u1 = m1(c)*img1(:,:,c) + c1(c);
+%     u2 = m2(c)*img1(:,:,c) + c2(c);
+%     bessel = bsxfun(@besseli,abs(d_x(:,:,c)),2*sqrt(u1.*u2));
+% 
+%     p = exp(-u1-u2).*(u1./u2).^(d_x(:,:,c)/2).*bessel;
     
-    edge_img(edge_mask) = 1;
-    edge_img(circshift(edge_mask,-1,2)) = 1;
+    edge_img(edge_mask_x(:,:,c)) = 1;
+    edge_img(circshift(edge_mask_x(:,:,c),-1,2)) = 1;
     
-    u1 = m1(c)*img1(:,:,c) + c1(c);
-    u2 = m2(c)*img1(:,:,c) + c2(c);
-    bessel = bsxfun(@besseli,abs(d_y(:,:,c)),2*sqrt(u1.*u2));
-
-    p = exp(-u1-u2).*(u1./u2).^(d_y(:,:,c)/2).*bessel;
-    edge_mask = p < 5e-10;
+%     u1 = m1(c)*img1(:,:,c) + c1(c);
+%     u2 = m2(c)*img1(:,:,c) + c2(c);
+%     bessel = bsxfun(@besseli,abs(d_y(:,:,c)),2*sqrt(u1.*u2));
+% 
+%     p = exp(-u1-u2).*(u1./u2).^(d_y(:,:,c)/2).*bessel;
     
-    edge_img(edge_mask) = 1;
-    edge_img(circshift(edge_mask,-1,1)) = 1;
+    edge_img(edge_mask_y(:,:,c)) = 1;
+    edge_img(circshift(edge_mask_y(:,:,c),-1,1)) = 1;
 end
 
 canny_edge = zeros(m,n);
@@ -72,5 +88,5 @@ title('Skellam Edge Image');
 figure; imshow(canny_edge);
 title('Canny Edge Image');
 
-
+figure, scatter(I_all(:,1),I_A(:,1));
 
--- Project/code/get_Ia.m
+++ Project/code/get_Ia.m
@@ -1,25 +1,35 @@
-function [out_id] = get_Ia(u1 ,u2, alpha)
+function [I_A] = get_Ia(u1 ,u2, alpha)
     diff = -255:1:255;
     N = length(diff);
     M = (N+1)/2;
 %     %p is of length N, M->0 
     max_val = 1 - alpha;
     p = zeros(size(diff));
-    for i = 1:length(diff)
-        p(i) = exp(-u1-u2)*(u1/u1)^(diff(i)/2)*besseli(abs(diff(i)),2*sqrt(u1*u2));
-    end
     
-    F = cumsum(p);
-    out_id = 1;
-    for i1 = 1:M-1
-        if (F(M-i1) - F(M+i1) < max_val)
-            out_id = i1;
+    [N,C] = size(u1);
+    I_A = zeros(N,C);
+    
+    for c = 1:C
+        for j = 1:N
+            for i = 1:length(diff)
+                p(i) = exp(-u1(j,c)-u2(j,c))*(u1(j,c)/u2(j,c))^(diff(i)/2)*besseli(abs(diff(i)),2*sqrt(u1(j,c)*u2(j,c)));
+            end
+
+            F = cumsum(p);
+            out_id = 1;
+            for i1 = 1:M-1
+                if (F(M+i1) - F(M-i1) < max_val)
+                    out_id = diff(M+i1);
+                end
+            end
+
+            I_A(j,c) = out_id;
+    %         figure;
+    %         plot(diff,p);
+    %         hold on;
+    %         plot(diff,F);
+    %         hold off;
         end
     end
-    figure;
-    plot(diff,p);
-    hold on;
-    plot(diff,F);
-    hold off;
    
 end
